d495acc2ccb2ff411a6faab545681471440cf7cf
sophiechen <348999970@gapps.yrdsb.ca>
2025-01-07 10:19:32 -0500

    created the project

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello World!");
+        System.out.println("I work at a bubble tea store");
+    }
+}
