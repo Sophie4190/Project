7ce593fc084abbc49dda4d12fb36cc1a441ac0f1
Sophie4190 <348999970@gapps.yrdsb.ca>
2025-01-08 15:27:15 +0000

    Co-authored-by: mkaushal123 <mkaushal123@users.noreply.github.com>

--- tbao/src/main.java
+++ tbao/src/main.java
@@ -4,5 +4,6 @@ public class main {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello World!");
         System.out.println("I work at a bubble tea store");
+        System.out.println("Baran looks so serious right now lets all stare at her and laugh");
     }
 }
