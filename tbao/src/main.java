8d9db1e3e3aef65a591a68e690c9b064f3529355
Sophie4190 <348999970@gapps.yrdsb.ca>
2025-01-09 23:45:12 +0000

    Start added

--- tbao/src/main.java
+++ tbao/src/main.java
@@ -2,8 +2,27 @@ import java.util.*;
 public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello World!");
-        System.out.println("I work at a bubble tea store");
-        System.out.println("Baran looks so serious right now lets all stare at her and laugh");
+        System.out.println("Welcome to Tbao! What type of products are you looking for today? You may choose from Clothes, Makeup or Accessories!");
+        String type = sc.nextLine().toLowerCase;
+        if (type.equals("clothes")){
+            System.out.println("What type of Clothes are you looking for? We have sweaters, pants, tees, sleepwear and sportswear.");
+
+
+
+        }
+        else if (type.equals("makeup")){
+            System.out.println("What type of makeup are you looking for? We have foundation, concealer, eyelashes, eyeshadow, lippies and blush.");
+
+        }
+        else if(type.equals("accessories")){
+            System.out.println("What type of accessories are you looking for? We have necklaces, earrings, rings, bracelets and anklets");
+
+        }
+        else{
+            System.out.println("Sorry we do not have that here..");
+        }
+            
+    
+
     }
 }
